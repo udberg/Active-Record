93426fddde26d0774d3c7a3c003f83ed80cef8ff
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-30 16:48:58 +0500

    fixed error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :body, presence: true, length: { maximum:200 }
+  validates :body, presence: true, length: { maximum: 200 }
 end
