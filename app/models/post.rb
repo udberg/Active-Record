1b568f9fee42bcd3b9022c1d1666f0161e14ef91
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-30 14:33:42 +0500

    added post model

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum:200 }
+end
