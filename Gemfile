56a6e8b279704feb67c32db1b1f912d75c6f8c26
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-31 15:42:50 +0500

    fixed linter config

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.6.5'
+ruby '2.7.2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.1.0'
@@ -22,7 +22,7 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-gem 'rubocop', '~>0.81.0'
+gem 'rubocop'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
 
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
+  # rubocop:disable Style/MutableConstant
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  #rubocop:disable AlignHash
+  # rubocop:enable Style/MutableConstant
+  # rubocop:disable Layout/HashAlignment
   validates :email, presence: true, length: { maximum: 244 },
-                                      format: { with: VALID_EMAIL_REGEX },
-                                      uniqueness: true
-  # rubocop:enable AlignHash 
+                                    format: { with: VALID_EMAIL_REGEX },
+                                    uniqueness: true
+  # rubocop:enable Layout/HashAlignment
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
